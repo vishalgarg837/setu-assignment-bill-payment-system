1553002473c57cd127e77b56300a046e1d769d30
Vishal Garg <vishal@fivetran.com>
2020-07-11 14:49:57 +0530

    add(BillPayment): added constant utility

---
+++ src/main/java/com/setu/assignment/util/Constants.java
@@ -0,0 +1,17 @@
+package com.setu.assignment.util;
+
+public class Constants {
+    public static final String API_KEY_HEADER = "X-API-KEY";
+    public static final String API_KEY = "f2717bab-2b22-4010-aad1-a5607f2c0b8e";
+
+    public static final String AUTH_ERROR = "auth-error";
+    public static final String CUSTOMER_NOT_FOUND = "customer-not-found";
+    public static final String INVALID_API_PARAMETERS = "invalid-api-parameters";
+    public static final String INVALID_REF_ID = "invalid-ref-id";
+    public static final String AMOUNT_MISMATCH = "amount-mismatch";
+    public static final String PATH_NOT_FOUND = "path-not-found";
+    public static final String UNHANDLED_ERROR = "unhandled-error";
+
+    public static final String SQL = "sql";
+    public static final String BILL_PAYMENT_SERVICE = "Bill Payment Service";
+}
