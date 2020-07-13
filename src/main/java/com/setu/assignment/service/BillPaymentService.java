681755575cae212c8ff96088f94349a113a3b906
vishalgarg837@gmail.com <vishalgarg837@gmail.com>
2020-07-13 20:00:21 +0530

    refactor(BillPayment): removed unused import statements

--- src/main/java/com/setu/assignment/service/BillPaymentService.java
+++ src/main/java/com/setu/assignment/service/BillPaymentService.java
@@ -1,11 +1,8 @@
 package com.setu.assignment.service;
 
 import com.setu.assignment.dao.BillPaymentDao;
-import com.setu.assignment.exception.InvalidApiParameters;
 import com.setu.assignment.model.BillData;
 import com.setu.assignment.model.PaymentUpdateRequest;
-import com.setu.assignment.util.Constants;
-import org.apache.commons.lang3.StringUtils;
 import org.skife.jdbi.v2.sqlobject.CreateSqlObject;
 
 public abstract class BillPaymentService {
